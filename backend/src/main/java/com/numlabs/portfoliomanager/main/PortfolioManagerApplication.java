b2b6adeca51bebf89f370cdc72d6698ed05497b9
nurhanrecep <nurhanrecep@gmail.com>
2018-06-15 19:15:43 +0300

    backend commit

---
+++ backend/src/main/java/com/numlabs/portfoliomanager/main/PortfolioManagerApplication.java
@@ -0,0 +1,12 @@
+package com.numlabs.portfoliomanager.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioManagerApplication.class, args);
+	}
+}
